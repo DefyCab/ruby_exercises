b5a184ffc7b74783b5c848af7c109403de64066c
DefyCab <love.bellinder@gmail.com>
2021-10-26 00:04:23 +0200

    Completed movie kata, very gratifying

---
+++ movies.rb
@@ -0,0 +1,10 @@
+movie_1 = {name: "Tillbaka till framtiden", year: 1985}
+movie_2 = {name: "Terminator", year: 1984}
+movie_3 = {name: "Dazed and confused", year: 1993}
+movie_4 = {name: "Lolita", year: 1962}
+
+movies = [movie_1[:year], movie_2[:year], movie_3[:year], movie_4[:year]]
+
+movies.each do |year|
+    puts year
+end
