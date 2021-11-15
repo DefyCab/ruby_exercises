7aebd556c604b005cdbac2e242a305c125b5fb90
DefyCab <love.bellinder@gmail.com>
2021-11-15 10:42:04 +0100

    updated with two simple arrays

---
+++ array_string_index.rb
@@ -0,0 +1,8 @@
+string = "Welcome to America!"
+puts a = string[6]   #e
+puts b = string[11] #A
+c = string[19] #nil
+#string are arrays of characters. "a" is the seventh character of the string, last 'e' of 
+#the word "Welcome".
+#"b" is the 12th character of the string, 'A'
+#"c" is "nil", empty, undefined. There is no 20th character in the string.
